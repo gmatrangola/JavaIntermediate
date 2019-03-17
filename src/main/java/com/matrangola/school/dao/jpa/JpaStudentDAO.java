019ff132242030dc895a86062ad99fcaf85ee5fc
Geoff Matrangola <geoff@matrangola.com>
2019-03-17 11:04:07 -0400

    Initial Commit

---
+++ src/main/java/com/matrangola/school/dao/jpa/JpaStudentDAO.java
@@ -0,0 +1,60 @@
+package com.matrangola.school.dao.jpa;
+
+import com.matrangola.school.dao.BaseDAO;
+import com.matrangola.school.domain.Student;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JpaStudentDAO implements BaseDAO<Student> {
+
+	private Map<Integer, Student> students = new HashMap<Integer, Student>();
+	private static int nextId = 0;
+	
+	@Override
+	public void update(Student updateObject) {
+		if(students.containsKey(updateObject.getId())) {
+			students.put(updateObject.getId(), updateObject);
+		}
+	}
+
+	@Override
+	public void delete(Student student) {
+		students.remove(student.getId());
+	}
+
+	@Override
+	public Student create(Student newObject) {
+		//Create a new Id
+		int newId = nextId++;
+		newObject.setId(newId);
+		newObject.setName("Jpa" + newObject.getName());
+		students.put(newId, newObject);
+		
+		return newObject;
+	}
+
+	@Override
+	public Student get(int id) {
+		return students.get(id);
+	}
+
+	@Override
+	public List<Student> getAll() {
+		return new ArrayList<Student>(students.values());
+	}
+	
+	public void deleteStore() {
+		students = null;
+	}
+	
+	public void createStore() {
+		students = new HashMap<Integer, Student>();
+	}
+
+	public Map<Integer, Student> getStudents() {
+		return students;
+	}
+}
